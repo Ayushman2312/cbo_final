57c7536f3110fd85f90a9a58d02815fbd70a2e02
Ayushman2312 <108574597+Ayushman2312@users.noreply.github.com>
2024-12-24 14:57:21 +0530

    Update settings.py

--- cbo/settings.py
+++ cbo/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'cbo.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    ‘default’: {
-        ‘ENGINE’: ‘django.db.backends.postgresql_psycopg2’,
-        ‘NAME’: ‘cbo’,
-        ‘USER’: ‘admin’,
-        ‘PASSWORD’: ‘Ayushman@2312’,
-        ‘HOST’: ‘localhost’,
-        ‘PORT’: ‘5432’, # default PostgreSQL port
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cbo',
+        'USER': 'admin',
+        'PASSWORD': 'Ayushman@2312',
+        'HOST': 'localhost',
+        'PORT': '5432', # default PostgreSQL port
     }
 }
 
